aa8b60e2f10f0efeed7e53f81ed65e15a5f2b915
Jonas Kunze <kunze.jonas@gmail.com>
2015-03-09 12:04:16 +0100

    Showing number of ratings in the snippet show view

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,8 @@
 class Snippet < ActiveRecord::Base
   has_and_belongs_to_many :rating_marks
   has_many :ratings
+
+  def getNumberOfRatings(ratingMarkID)
+    return Rating.count({snippet_id: id, rating_mark_id: ratingMarkID})
+  end
 end
