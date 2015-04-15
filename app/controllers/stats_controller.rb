b619a3f5fee3cb5ad121121853fcfc3c93d61c8d
Jonas Kunze <kunze.jonas@gmail.com>
2015-04-15 12:17:47 +0200

    added link to stats in menu

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,9 +1,11 @@
 class StatsController < ApplicationController
   def json
     onlineTimeout = 60 # number of seconds a user is assumed to be online
-  
+
+    # The user requesting this statistics is also online  
     UsersController.updateSession(session)
 
+    # count then umber of users that have been active within the last 'onlineTimeout' seconds
     onlineUsers = User.where("lastActivityTime >= ?", Time.zone.now-onlineTimeout).count
 
     jsonMap = {};
