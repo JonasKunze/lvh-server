71ec79e7167b0d18ea171dc60444c90721a61215
Jonas Kunze <kunze.jonas@gmail.com>
2015-04-15 19:02:29 +0200

    Unique vote implemented: If vote already exists it's rating mark value is only updated

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,7 +2,7 @@ class RatingsController < ApplicationController
   def index
     @ratings = Rating.all
   end
-  
+
   def show
     @rating = Rating.find(params[:id])
   end
@@ -14,12 +14,20 @@ class RatingsController < ApplicationController
   def edit
     @rating = Rating.find(params[:id])
   end
-  
+
   def create
     sessionUser = UsersController.getSessionUser(session)
     @snippet = Snippet.find(params[:snippet_id])
-    @rating = Rating.find({:snippet_id => @snippet.id, :user_id => sessionUser.id }) rescue nil
-
+    #@rating = Rating.take({:snippet_id => @snippet.id, :user_id => sessionUser.id }) rescue nil
+    @rating = Rating.where({:snippet_id => @snippet.id, :user_id => sessionUser.id }).take
+    puts "Snippet:"
+    puts @snippet.inspect
+    puts "Rating:"
+    puts @rating.inspect
+    puts "User ID:" 
+    puts sessionUser.id
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!"
+ 
     dataHash = rating_params.merge({:user_id => sessionUser.id})
     sucess = false
     if @rating
@@ -32,33 +40,34 @@ class RatingsController < ApplicationController
         success = true
       end
     end
-    
+
     if success
       redirect_to @rating
     else
       render 'new'
     end
   end
-  
+
   def update
     @rating = Rating.find(params[:id])
-    
+
     if @rating.update(rating_params)
       redirect_to @rating
     else
       render 'edit'
     end
   end
-  
+
   def destroy
     @rating = Rating.find(params[:id])
     @rating.destroy
-    
+
     redirect_to ratings_path
   end
-  
+
   private
-    def rating_params
-      params.require(:rating).permit(:rating_mark_id, :snippet_id)
-    end
+
+  def rating_params
+    params.require(:rating).permit(:rating_mark_id, :snippet_id)
+  end
 end
